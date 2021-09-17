7fabbeed6076d2b7e67ec518b37c49a0f20e6521
heptaluan <vd2o@foxmail.com>
2021-09-17 14:52:16 +0800

    update

--- src/components/common/MarkNoduleTool/MarkNoduleTool.js
+++ src/components/common/MarkNoduleTool/MarkNoduleTool.js
@@ -1,101 +1,221 @@
+import toolStyle from './toolStyle.js'
+import toolColors from './toolColors.js'
 import csTools from 'cornerstone-tools'
-const BaseTool = csTools.importInternal('base/BaseTool')
+import cornerstone from 'cornerstone-core'
 
-const fillRectangle = (evt, operationData, inside = true) => {
-  const { points, segmentationMixinType } = operationData
+const BaseAnnotationTool = csTools.importInternal('base/BaseAnnotationTool')
 
-  const { image } = evt.detail
-  const vertices = points.map(a => [a.x, a.y])
-  const [topLeft, bottomRight] = getBoundingBoxAroundPolygon(vertices, image)
+function getToolState(element, toolName) {
+  const toolStateManager = getElementToolStateManager(element)
 
-  fillInsideShape(evt, operationData, () => true, topLeft, bottomRight)
+  return toolStateManager.get(element, toolName)
 }
 
-const getBoundingBoxAroundPolygon = (vertices, image) => {
-  let xMin = Infinity
-  let xMax = 0
-  let yMin = Infinity
-  let yMax = 0
-  const { width, height } = image
+function getElementToolStateManager(element) {
+  const enabledElement = cornerstone.getEnabledElement(element)
 
-  vertices.forEach(v => {
-    xMin = Math.min(v[0], xMin)
-    xMax = Math.max(v[0], xMax)
-    yMin = Math.min(v[1], yMin)
-    yMax = Math.max(v[1], yMax)
+  return enabledElement.toolStateManager
+}
+
+function getNewContext(canvas) {
+  const context = canvas.getContext('2d')
+  context.setTransform(1, 0, 0, 1, 0, 0)
+  return context
+}
+
+function draw(context, fn) {
+  context.save()
+  fn(context)
+  context.restore()
+}
+
+function rotatePoint(point, center, angle) {
+  const angleRadians = angle * (Math.PI / 180) // Convert to radians
+
+  const rotatedX = Math.cos(angleRadians) * (point.x - center.x) - Math.sin(angleRadians) * (point.y - center.y) + center.x
+
+  const rotatedY = Math.sin(angleRadians) * (point.x - center.x) + Math.cos(angleRadians) * (point.y - center.y) + center.y
+
+  return {
+    x: rotatedX,
+    y: rotatedY,
+  }
+}
+
+function path(context, options = {}, fn) {
+  const { color, lineWidth, fillStyle, lineDash, shouldDrawLines = true } = options
+
+  context.beginPath()
+  context.strokeStyle = color || context.strokeStyle
+
+  context.lineWidth = lineWidth || (lineWidth === undefined && toolStyle.getToolWidth()) || context.lineWidth
+  if (lineDash) {
+    context.setLineDash(lineDash)
+  }
+
+  fn(context)
+
+  if (fillStyle) {
+    context.fillStyle = fillStyle
+    context.fill()
+  }
+
+  if (shouldDrawLines) {
+    context.stroke()
+  }
+
+  if (lineDash) {
+    context.setLineDash([])
+  }
+}
+
+function drawRect(context, element, corner1, corner2, options, coordSystem = 'pixel', initialRotation = 0.0) {
+  if (coordSystem === 'pixel') {
+    corner1 = cornerstone.pixelToCanvas(element, corner1)
+    corner2 = cornerstone.pixelToCanvas(element, corner2)
+  }
+
+  const viewport = cornerstone.getViewport(element)
+
+  // Calculate the center of the image
+  const { clientWidth: width, clientHeight: height } = element
+  const { scale, translation } = viewport
+  const rotation = viewport.rotation - initialRotation
+
+  const centerPoint = {
+    x: width / 2 + translation.x * scale,
+    y: height / 2 + translation.y * scale,
+  }
+
+  if (Math.abs(rotation) > 0.05) {
+    corner1 = rotatePoint(corner1, centerPoint, -rotation)
+    corner2 = rotatePoint(corner2, centerPoint, -rotation)
+  }
+
+  const w = Math.abs(corner1.x - corner2.x)
+  const h = Math.abs(corner1.y - corner2.y)
+
+  corner1 = {
+    x: Math.min(corner1.x, corner2.x),
+    y: Math.min(corner1.y, corner2.y),
+  }
+
+  corner2 = {
+    x: corner1.x + w,
+    y: corner1.y + h,
+  }
+
+  let corner3 = {
+    x: corner1.x + w,
+    y: corner1.y,
+  }
+
+  let corner4 = {
+    x: corner1.x,
+    y: corner1.y + h,
+  }
+
+  if (Math.abs(rotation) > 0.05) {
+    corner1 = rotatePoint(corner1, centerPoint, rotation)
+    corner2 = rotatePoint(corner2, centerPoint, rotation)
+    corner3 = rotatePoint(corner3, centerPoint, rotation)
+    corner4 = rotatePoint(corner4, centerPoint, rotation)
+  }
+
+  path(context, options, context => {
+    context.moveTo(corner1.x, corner1.y)
+    context.lineTo(corner3.x, corner3.y)
+    context.lineTo(corner2.x, corner2.y)
+    context.lineTo(corner4.x, corner4.y)
+    context.lineTo(corner1.x, corner1.y)
   })
-
-  xMin = Math.floor(xMin)
-  yMin = Math.floor(yMin)
-  xMax = Math.floor(xMax)
-  yMax = Math.floor(yMax)
-
-  xMax = Math.min(width, xMax)
-  xMin = Math.max(0, xMin)
-  yMax = Math.min(height, yMax)
-  yMin = Math.max(0, yMin)
-
-  return [
-    [xMin, yMin],
-    [xMax, yMax],
-  ]
 }
 
-const fillInsideShape = (evt, operationData, pointInShape, topLeft, bottomRight) => {
-  fillShape(evt, operationData, pointInShape, topLeft, bottomRight, 'inside')
-}
+export default class MarkNoduleTool extends BaseAnnotationTool {
+  constructor(props = {}) {
+    const defaultProps = {
+      name: 'MarkNodule',
+      supportedInteractionTypes: ['Mouse'],
+      configuration: {
+        drawHandles: false,
+        drawHandlesOnHover: false,
+        hideHandlesIfMoving: true,
+        renderDashed: false,
+      },
+    }
 
-const fillShape = (evt, operationData, pointInShape, topLeft, bottomRight, insideOrOutside = 'inside') => {
-  debugger
-  const { pixelData, segmentIndex } = operationData
+    super(props, defaultProps)
+  }
 
-  const { width } = evt.detail.image
-  const [xMin, yMin] = topLeft
-  const [xMax, yMax] = bottomRight
+  handleSelectedCallback(e) {
+    // 覆盖父类方法，禁止工具选中
+  }
 
-  for (let x = xMin; x < xMax; x++) {
-    for (let y = yMin; y < yMax; y++) {
-      const pixelIndex = y * width + x
-
-      // If the pixel is the same segmentIndex and is inside the
-      // Region defined by the array of points, set their value to segmentIndex.
-      if (
-        pointInShape({
-          x,
-          y,
-        })
-      ) {
-        pixelData[pixelIndex] = segmentIndex
-      }
+  createNewMeasurement(eventData) {
+    return {
+      visible: true,
+      active: false,
+      color: undefined,
+      invalidated: true,
+      handles: {
+        start: {
+          x: eventData.currentPoints.image.x,
+          y: eventData.currentPoints.image.y,
+          highlight: false,
+          active: false,
+        },
+        end: {
+          x: eventData.currentPoints.image.x,
+          y: eventData.currentPoints.image.y,
+          highlight: false,
+          active: false,
+        },
+      },
     }
   }
-}
 
-const fillInsideRectangle = (evt, operationData) => {
-  fillRectangle(evt, operationData, true)
-}
+  pointNearTool(element, data, coords, interactionType) {
+    return false
+  }
 
-export default class MarkNoduleTool extends BaseTool {
-  constructor(name = 'MarkNodule') {
-    super({
-      name,
-      supportedInteractionTypes: ['Mouse'],
-      mixins: ['rectangleSegmentationMixin'],
-      strategies: {
-        default: fillInsideRectangle,
-      },
+  updateCachedStats(image, element, data) {
+    // const seriesModule = cornerstone.metaData.get('generalSeriesModule', image.imageId) || {}
+    // const modality = seriesModule.modality
+    // const pixelSpacing = getPixelSpacing(image)
+    // const stats = _calculateStats(image, element, data.handles, modality, pixelSpacing)
+    // data.cachedStats = stats
+    // data.invalidated = false
+  }
+
+  renderToolData(evt) {
+    const toolData = getToolState(evt.currentTarget, this.name)
+
+    if (!toolData) {
+      return
+    }
+
+    const eventData = evt.detail
+    const { image, element } = eventData
+    const context = getNewContext(eventData.canvasContext.canvas)
+
+    draw(context, context => {
+      // If we have tool data for this element - iterate over each set and draw it
+      for (let i = 0; i < toolData.data.length; i++) {
+        const data = toolData.data[i]
+
+        if (data.visible === false) {
+          continue
+        }
+
+        // Configure
+        const color = toolColors.getColorIfActive(data)
+
+        const rectOptions = { color }
+
+        // Draw
+        drawRect(context, element, data.handles.start, data.handles.end, rectOptions, 'pixel', data.handles.initialRotation)
+      }
     })
   }
-
-  preMouseDownCallback(evt) {
-    console.log('Hello cornerstoneTools!')
-  }
-
-  activeCallback(element) {
-    console.log(`Hello element ${element.uuid}!`)
-  }
-
-  disabledCallback(element) {
-    console.log(`Goodbye element ${element.uuid}!`)
-  }
+  
 }
